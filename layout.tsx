76fc8d9acc3dc1346bf0ecb9faa8596f63ab648b
narphinugan <mjjefferies@gmail.com>
2026-04-23 09:37:37 -0400

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html>
-      <body style={{ fontFamily: 'sans-serif', padding: 20 }}>
-        {children}
-      </body>
-    </html>
-  );
-}
