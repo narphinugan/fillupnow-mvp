69928229d57d961eb1d36a24bea1a8f58981fb48
narphinugan <mjjefferies@gmail.com>
2026-04-23 09:35:35 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,10 @@
 
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body style={{ fontFamily: 'sans-serif', padding: 20 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
